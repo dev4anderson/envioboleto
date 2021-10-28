8d9366e38478dc8ab7820e472dd307ce50b908a1
ANDERSON MACEDO DOS SANTOS <andersons_meta@3coracoes.com.br>
2021-10-28 09:14:35 -0300

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,37 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Tue Oct 26 2021 10:29:34 GMT-0300 (Horário Padrão de Brasília)|
+|**App Generator**<br>@sap/generator-fiori|
+|**App Generator Version**<br>1.3.6|
+|**Generation Platform**<br>Visual Studio Code|
+|**Floorplan Used**<br>List Report Object Page V2|
+|**Service Type**<br>SAP System (ABAP On Premise)|
+|**Service URL**<br>https://fiori-dev.meta.com.br/sap/opu/odata/sap/ZUI_O2_ENVIO_BOLETO/
+|**Module Name**<br>envioboleto|
+|**Application Title**<br>Envio/Download Boleto|
+|**Namespace**<br>br.com.meta|
+|**UI5 Theme**<br>sap_fiori_3|
+|**UI5 Version**<br>Latest|
+|**Enable Code Assist Libraries**<br>False|
+|**Add Eslint configuration**<br>False|
+|**Enable Telemetry**<br>True|
+|**Main Entity**<br>ZC_FI_ENVIA_BOLETO|
+
+## envioboleto
+
+A Fiori application.
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,68 @@
+sap.ui.define([],
+
+function (){
+    "use strict";
+    return {
+        showPDF: function(oEvent) {
+/* =================================================================== 
+    Recupera a linha selecionada 
+=================================================================== */
+            var oSelection = this.extensionAPI.getSelectedContexts();
+
+            if(oSelection.length > 0){
+                var vChave =  oSelection.reduce(function(sText,oSelectedContext){
+                    var mSelectedData = oSelectedContext.getObject();
+
+                    return sText + 
+                           "empresa='" + mSelectedData.Empresa +"'," + 
+                           "documento='" + mSelectedData.NumDocumento +"'," +
+                           "exercicio='" + mSelectedData.Exercicio +"'," + 
+                           "item='00" + mSelectedData.ItemDocumento +"'" ;
+                }, "");
+            }    
+
+/* =================================================================== 
+    Chama serviço gateway (SEGW) 
+=================================================================== */
+
+            var vServiceURL = "/sap/opu/data/SAP/ZFI_BOLETO_SRV/";
+            var oModel = new sap.ui.model.odata.ODataModel(vServiceURL, false);
+
+            var vServiceUrlRead = "pdfset("+ vChave + ")/$value";
+
+            oModel.read(vServiceUrlRead,null,null,true, 
+/* =================================================================== 
+    Em caso de sucesso, Chama PDF Viewer com o novo link 
+=================================================================== */                
+                function(oData){
+
+                    var sSource = '' + oData.url;
+                    var opdfViewer = new sap.m.PDFViewer    (); 
+                    this.getView().addDependent(opdfViewer); 
+                    opdfViewer.setSource(sSource); 
+                    opdfViewer.setTitle("My PDF"); 
+                    opdfViewer.open();
+                }.bind(this),
+
+                function(error){
+                    alert('Falha ao buscar arquivo');
+                })
+
+            
+        }
+    };
+});
+
+
+// sap.ui.define([],
+//     function (){
+//         "use strict";
+//         return sap.ui.controller(br.com.meta.envioboleto.ext.controller.ListReportExt, {
+//             showPDF: function(oEvent) {
+//                 alert('showPDF');
+//             }
+//         });
+//     });
+
+
+    
